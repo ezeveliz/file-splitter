d193b59439321a1537d81432dea684fb2babfdd4
ezeveliz <ezequiel_veliz@hotmail.com>
2022-09-13 16:39:20 -0300

    * Agrego una validación para la cantidad de líneas ingresadas, antes se podía ingresar un número menor a 1, lo cual no tiene sentido. * Paso a utilizar la biblioteca csv de Python para interpretar el archivo a leer, esta me soluciona el problema de los saltos de línea en lugares inesperados. * Antes no cerraba el puntero que apuntaba al archivo abierto, corregido. * Agrego comentarios explicativos.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import csv
 
 # Tamaño por default de los archivos a crear
 default_size = 500000
@@ -14,9 +15,11 @@ extension = ""
 
 # Escribo cada parte del archivo
 def write_chunk(part, lines):
-    with open(new_filename + '_' + str(part) + extension, 'w') as f_out:
-        f_out.write(header)
-        f_out.writelines(lines)
+    with open(new_filename + '_' + str(part) + extension, 'w') as file:
+        file_writer = csv.writer(file, delimiter=",")
+
+        file_writer.writerow(header)
+        file_writer.writerows(lines)
 
 
 # Verifico los parámetros pasados y comienzo a procesar el archivo original
@@ -46,28 +49,40 @@ def main(args):
     # Verifico si pasaron el parámetro de tamaño de archivo deseado, si no lo pasaron, será 500K lineas
     try:
         filesize = int(args[2])
+        if filesize < 1:
+            print("¿En serio?")
+            return
     except:
         filesize = default_size
 
     print("Iniciando división...\n")
 
+    # Esto sirve para interpretar el archivo original como un csv, de esta manera me
+    # ahorro el problema de saltos de línea en lugares inesperados
+    file_reader = csv.reader(file, delimiter=",")
+
     count = 0
     part = 0
-    header = file.readline()
+    # Obtengo la primera línea(header) para colocarlo en todas las partes
+    header = next(file_reader)
     lines = []
-    for line in file:
+    for line in file_reader:
         count += 1
         lines.append(line)
+        # Se completó el buffer con la cantidad de líneas especificadas en filesize, lo
+        # vuelco en un archivo
         if count % filesize == 0:
             print("Creando parte " + str(part + 1) + "...")
             write_chunk(part, lines)
             lines = []
             part += 1
-    # write remainder
+    # Vuelco las líneas restantes en un archivo, en este caso no llegamos a completar el
+    # buffer con la cantidad especificada en filesize
     if len(lines) > 0:
         print("Creando parte " + str(part + 1) + "...\n")
         write_chunk(part, lines)
     print("El archivo fue dividido en " + str(part + 1) + " partes en el directorio " + os.path.dirname(filename))
+    file.close()
 
 
 # Press the green button in the gutter to run the script.
